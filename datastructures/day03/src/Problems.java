5b60e913e926c7d7c58c203f912034576596e0ba
AllisonBasore <allison.basore@students.olin.edu>
2018-02-04 13:45:55 -0500

    added beginning of day 3

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -13,20 +13,77 @@ public class Problems {
     }
 
     public static List<Integer> removeKDigits(int[] A, int k) {
-        // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
-        List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
-        return l;
+        // Need to test and figure it out
+        int outputTotal = 0;
+        List<Integer> outputList = new LinkedList<>();
+        int desiredLength = A.length-k;
+        while(outputTotal < desiredLength){
+            int minimumIndex = 0;
+            for(int i = 0; i <= k; i++) {
+                if (A[i] < A[minimumIndex]) {
+                    minimumIndex = i;
+                }
+            }
+            outputList.add(A[minimumIndex]);
+            outputTotal ++;
+            A = Arrays.copyOfRange(A, minimumIndex+1, A.length);
+            k = k - minimumIndex;
+            if(k <= 0){
+                for(int b = 0 ; b < A.length; b++){
+                    outputList.add(A[b]);
+                }
+
+                return outputList;
+            }
+        }
+        return outputList;
+
     }
 
+
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        // 1) find length with one fast/ one slow method
+        // 2) See if there is a mid node
+        // 3) Reverse First half
+        // 4) Compare halfs
+
+        if(n == null){
+            return true;
+        }
+
+        Node slowNode = n;
+        Node fastNode = n;
+        int count = 0;
+
+        while(fastNode.next != null){
+            slowNode = slowNode.next;
+            fastNode = fastNode.next;
+            count ++;
+        }
+
+        // TODO determine if there is a mid Node
+
+        output = compare(reverse(n, slowNode), slowNode, fastNode);
+
+
+        return output;
     }
 
     public static String infixToPostfix(String s) {
-        // TODO
+        // Run trhough and identify entires/ partial entries
+        // identify the operator
+        // put the operator on right of entry
+        // set entitty as a partial entry
+
+        // define an entry as one quntity, operator, other quantity
+
+        for(int i; i< s.length(); i++){
+            System.out.println(i);
+        }
+
+
+
+
         return null;
     }
 
