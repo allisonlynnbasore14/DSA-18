790968c2fe065219fe55b1a40c7a085eab40dfbc
AllisonBasore <allison.basore@students.olin.edu>
2018-02-24 17:01:44 -0500

    added sorting day 3

--- sorting/day03/src/CountingSort.java
+++ sorting/day03/src/CountingSort.java
@@ -2,12 +2,35 @@ public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: O(n) + O(n+K) --> O(n+K), where k = the max value in the array
      *
      * k: maximum element in array A
      */
     static void countingSort(int[] A) {
-        // TODO
+
+        int max = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+
+        int[] newArray = new int[max+1];
+        for(int i = 0; i < A.length; i++){
+            int index = A[i];
+            newArray[index] = newArray[index] + 1;
+        }
+        int count = 0;
+        for(int j = 0; j < newArray.length; j++){
+            //if(newArray[j] != 0){
+                // j is the index of the new array and the value that is kept
+                for(int p = newArray[j]; p>0; p-- ){
+                    A[count] = j;
+                    count++;
+                }
+            //}
+        }
     }
 
 }
--- sorting/day03/src/RadixSort.java
+++ sorting/day03/src/RadixSort.java
@@ -22,11 +22,60 @@ public class RadixSort {
         for (int i = 0; i < b; i++)
             L[i] = new LinkedList<>();
         for (int i : A) {
+            int digit = 0;
+            int temp = i;
+            int count = n;
+            // When n = 4, I want to do temp % 10
+            // When n = 3, I want to do temp / 10 % 10
+            // When n = 2, I want to do temp /10/10 % 10
+            // When n = 1; I want to do temp /10/10/10 %10
+
+            while (count > 0) {
+                if(temp < 10 && count != 0){
+                    digit = 0;
+                    L[digit].add(i);
+                    count = 0;
+                }else if(count == 0){
+                    digit = temp % 10;
+                    L[digit].add(i);
+                    count = 0;
+                }else{
+                    temp = temp / 10;
+                    count--;
+                }
+            }
+
+
+            // If temp/10
+/*            if(temp < 10){
+                // if there is only one number??
+                digit = temp;
+            }else{
+            }*/
+/*
+
+            String s = String.valueOf(temp);
+            if(n > s.length()){
+                L[0].add(i);
+            }else{
+                char ch = s.charAt(w-n);
+                L[digit].add((int) ch);
+            }
+*/
+
             // TODO: Extract the relevant digit from i, and add i to the corresponding Linked List.
+//            System.out.println(digit);
+//            System.out.println("ff");
+//            System.out.println(L.length);
+
         }
         int j = 0; // index in A to place numbers
         for (LinkedList<Integer> list : L) {
             // TODO: Put all numbers in the linked lists into A
+            for(int p = list.size()-1; p >= 0; p--){
+                A[j] = list.get(p);
+                j++;
+            }
         }
     }
 
@@ -38,13 +87,22 @@ public class RadixSort {
      *
      * @param b The base to use for radix sort
      */
-    static void radixSort(int[] A, int b) {
+    static void radixSort(int[] A, int b) { ;
         // Calculate the upper-bound for numbers in A
         int k = A[0] + 1;
         for (int i = 1; i < A.length; i++)
             k = (A[i] + 1 > k) ? A[i] + 1 : k;
         int w = (int) Math.ceil(Math.log(k) / Math.log(b)); // w = log base b of k, word length of numbers
+        System.out.println(w);
+        System.out.println("ss");
         // TODO: Perform radix sort
+        for(int m = w; m >= 0 ; m --){
+            countingSortByDigit(A, k, m);
+            for(int dd = A.length-1; dd > A.length-100; dd --) {
+                System.out.println(A[dd]);
+            }
+            System.out.println("00000000000000000000000000000");
+        }
     }
 
 }
