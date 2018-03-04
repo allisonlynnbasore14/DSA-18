e02d5b5418ee5775a0681fddc8a6e74e72d3248e
AllisonBasore <allison.basore@students.olin.edu>
2018-03-03 21:39:00 -0500

    added pre and succ code for binary tree

--- trees/day00/src/BinarySearchTree.java
+++ trees/day00/src/BinarySearchTree.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinarySearchTree<T extends Comparable<T>> {
@@ -28,8 +29,26 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
     public List<T> inOrderTraversal() {
-        // TODO
-        return null;
+        return inOrdert(root);
+    }
+
+
+    public List<T> inOrdert(TreeNode<T> n){
+        // go to left then root then right
+        List<T> list = new ArrayList<T>();
+
+        //Base Case
+
+        if(n == null){
+            return list;
+        }
+
+        list.addAll(inOrdert(n.leftChild));
+        list.add(n.key);
+        list.addAll(inOrdert(n.rightChild));
+
+        return list;
+
     }
 
     /**
@@ -95,14 +114,128 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return null;
     }
 
+/*    private TreeNode<T> findPredecessorIter(TreeNode<T> n, T key) {
+        TreeNode<T> target = n;
+        if(target == null){
+            return target;
+        }
+        return target;
+    }*/
+
     private TreeNode<T> findPredecessor(TreeNode<T> n) {
-        // TODO
+        // Needs to be a recursive thing that
+        // Base case of null
+        // 1) The right most node on your left sideS
+        // 2) if key is < root, sucesss = root and , search recrusivle into left
+
+
+        TreeNode<T> target = n;
+
+        if (target == null) {
+            return target;
+        }
+
+
+        if (n.leftChild != null) {
+            target = n.leftChild;
+            while (target != null) {
+
+
+                System.out.println(target);
+                if (target.rightChild == null) {
+                    //System.out.println(target);
+                    return target;
+                }
+                target = target.rightChild;
+            }
+            return target;
+        }
+
+/*        if (root.key.compareTo(target.key) >= 0) {
+            return findPredecessor(root.leftChild);
+        }*/
+
+        target = n;
+        while(target.parent != null){
+            if(target.parent.key.compareTo(n.key) <=0){
+                // if the parent is smaller
+                return target.parent;
+            }else{
+                target = target.parent;
+            }
+        }
         return null;
+
     }
 
+/*        if(target.parent != null){
+            if(target.parent.key.compareTo(target.key) <= 0){
+                return target.parent;
+            }else if(target.leftChild == null){
+                if(target.parent.parent != null) {
+                    if(target.parent.parent.key.compareTo(target.parent.key) <=0){
+                        System.out.println(target.parent);
+                        System.out.println("ddd");
+                        return target.parent.parent;
+                    }
+                }
+            }
+        }
+        if(n.leftChild != null) {
+            target = n.leftChild;
+
+        }else{
+
+
+
+            if( n.parent.key.equals(7)){
+                System.out.println("lll");
+            }
+            return null;
+        }
+
+
+
+       // System.out.println(target);
+        return target;
+    }*/
+
     private TreeNode<T> findSuccessor(TreeNode<T> n) {
-        // TODO
+
+        TreeNode<T> target = n;
+
+        if (target == null) {
+            return target;
+        }
+
+
+        if (n.rightChild != null) {
+            target = n.rightChild;
+            while (target != null) {
+                if (target.leftChild == null) {
+                    //System.out.println(target);
+                    return target;
+                }
+                target = target.leftChild;
+            }
+            return target;
+        }
+
+/*        if (root.key.compareTo(target.key) >= 0) {
+            return findPredecessor(root.leftChild);
+        }*/
+
+        target = n;
+        while(target.parent != null){
+            if(target.parent.key.compareTo(n.key) >=0){
+                // if the parent is smaller
+                return target.parent;
+            }else{
+                target = target.parent;
+            }
+        }
         return null;
+
     }
 
     /**
