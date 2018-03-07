dc47ca4d9a39585babd756219c20e7d3b16eedfa
AllisonBasore <allison.basore@students.olin.edu>
2018-03-07 10:37:23 -0500

    adding first draft of tree day 1

--- trees/day01/src/AVLTree.java
+++ trees/day01/src/AVLTree.java
@@ -7,9 +7,15 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     TreeNode<T> delete(TreeNode<T> n, T key) {
         n = super.delete(n, key);
         if (n != null) {
-            // TODO
             // update the height of the tree using the height of the left and right child
+            if(n.rightChild.height > n.leftChild.height){
+                n.height = n.rightChild.height + 1;
+            }else{
+                n.height = n.leftChild.height + 1;
+            }
+
             // return balance(n)
+            return balance(n);
         }
         return null;
     }
@@ -20,10 +26,21 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     @Override
     TreeNode<T> insert(TreeNode<T> n, T key) {
         n = super.insert(n, key);
+        n.height = height(n);
         if (n != null) {
-            // TODO
             // update the height of the tree using the height of the left and right child
+            if(n.rightChild != null ){
+                if(n.leftChild != null){
+                    if(n.rightChild.height < n.leftChild.height){
+                        n.height = n.leftChild.height + 1;
+                        return balance(n);
+                    }
+                }
+                n.height = n.rightChild.height + 1;
+            }
+
             // return balance(n)
+            return balance(n);
         }
         return null;
     }
@@ -43,8 +60,33 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     // Return the height of the given node. Return -1 if null.
     private int height(TreeNode<T> n) {
-        // TODO
-        return 0;
+        // Recursively call n.child height until there are no children and return the heights each time adding one
+        // At the end, keep which is higher
+        // Base case is when the child is null
+
+        TreeNode<T>  rC = n.rightChild;
+        TreeNode<T>  lC = n.leftChild;
+        if(rC == null && lC== null){
+            return 1;
+        }
+
+        int left = 0;
+        int right = 0;
+
+        if(rC != null){
+            right  = height(rC);
+        }
+
+        if(lC != null){
+            left  = height(lC);
+        }
+
+        if(right > left){
+            return right + 1;
+        }else{
+            return left + 1;
+        }
+
     }
 
     public int height() {
@@ -53,8 +95,48 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     // Restores the AVL tree property of the subtree. Return the head of the new subtree
     TreeNode<T> balance(TreeNode<T> n) {
-        // TODO: (if you're having trouble, use pseudocode provided in slides)
-        return null;
+
+        // recursively check for the balance factor of every node
+        // if it is a very unbalanced thing, check the four cases
+        // once you are through them all return the root
+
+        // Base Case: when children == null
+        TreeNode<T>  rC = n.rightChild;
+        TreeNode<T>  lC = n.leftChild;
+/*        if(rC == null && lC== null){
+            return root;
+        }
+        if(rC == null){
+            return balance(lC);
+        }
+        if(lC == null){
+            return balance(rC);
+        }
+
+        if (balance(n) == root){*/
+        int left = 0;
+        int right = 0;
+
+        // check cases
+        if (balanceFactor(n) >= 2) {
+            if (right >= 0) {
+                rotateLeft(n);
+            } else if (right < 0) {
+                rotateRight(rC);
+                rotateLeft(n);
+            }
+        }
+
+        if (balanceFactor(n) <= -2) {
+            if (right <= 0) {
+                rotateRight(n);
+            } else if (right > 0) {
+                rotateLeft(rC);
+                rotateRight(n);
+            }
+        }
+
+        return n;
     }
 
     /**
@@ -65,23 +147,62 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
      * most one.
      */
     private int balanceFactor(TreeNode<T> n) {
-        // TODO
-        return 0;
+        int hL = -1;
+        if(n.leftChild != null){
+            hL = height(n.leftChild);
+        }
+        int hR = -1;
+        if(n.rightChild != null){
+            hR = height(n.rightChild);
+        }
+
+        return hR - hL;
     }
 
     /**
      * Perform a right rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateRight(TreeNode<T> n) {
-        // TODO
-        return null;
+        TreeNode<T> b = null;
+        TreeNode<T> x = null;
+        if(n!= null && n.hasRightChild()){
+            x = n.rightChild;
+        }
+        if(x != null && x.hasLeftChild()){
+            b = x.leftChild;
+        }
+
+        if(x!= null){
+            x.leftChild = n;
+        }
+
+        if(n!= null){
+            n.rightChild = b;
+        }
+        return n;
     }
 
     /**
      * Perform a left rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateLeft(TreeNode<T> n) {
-        // TODO
-        return null;
+
+        TreeNode<T> b = null;
+        TreeNode<T> x = null;
+        if(n!= null && n.hasLeftChild()){
+            x = n.leftChild;
+        }
+        if(x != null && x.hasRightChild()){
+            b = x.rightChild;
+        }
+
+        if(x!= null){
+            x.rightChild = n;
+        }
+
+        if(n!= null){
+            n.leftChild = b;
+        }
+        return n;
     }
 }
--- trees/day01/src/Problems.java
+++ trees/day01/src/Problems.java
@@ -1,7 +1,170 @@
+
 public class Problems {
 
     public static int leastSum(int[] A) {
-        //TODO
-        return 0;
+
+        // with radix sort everything has to be backwards now
+
+        if(A.length < 4) {
+            int sum = 0;
+            for (int k = 0; k < A.length; k++) {
+                sum = A[k] + sum;
+            }
+            return sum;
+        }
+        //HeapSort hs = new HeapSort();
+        //hs.heapify(A);
+        RadixSort.radixSort(A, A.length);
+//
+        if(A.length == 5){
+            int first = A[0]*100 + A[2]*10 + A[4];
+            int second = A[1]*10+ A[3];
+
+            return first + second;
+        }
+//
+//        if(A.length == 4){
+//            int a = A.length-1;
+//            int b = A.length-3;
+//            int first = A[a]*10 + A[b];
+//            int second = A[a-1]*10+ A[b-1]*1;
+//
+//            return first + second;
+//        }
+
+        if(A.length %2 == 0){
+            // is even
+            int sum1 = 0;
+            int sum2 = 0;
+            double L = (A.length/2)-1;
+            int m1 = (int) Math.pow(10.0, L);
+            int m2 = (int) Math.pow(10.0, L);
+            for(int i = 0; i<A.length-1;i++){
+                sum1 = sum1 + A[i]*m1;
+                i++;
+                sum2 = sum2 + A[i]*m2;
+                m1 = m1/10;
+                m2 = m2/10;
+            }
+            return sum1 + sum2;
+        }else{
+            // is odd
+            int sum1 = 0;
+            int sum2 = 0;
+            double L = (A.length/2)-1;
+            int m1 = (int) Math.pow(10.0, L);
+            int m2 = (int) Math.pow(10.0, L-1);
+            for(int i = 0; i<A.length-1;i++){
+                sum1 = sum1 + A[i]*m1;
+                i++;
+                if(i == A.length){
+                    return sum1 + sum2;
+                }
+                sum2 = sum2 + A[i]*m2;
+                m1 = m1/10;
+                m2 = m2/10;
+            }
+            return sum1 + sum2;
+        }
+
+/*        if(A.length < 7){
+            int a = A.length-1;
+            int b = A.length-3;
+            int c = A.length-5;
+            if(A[b]<A[a-1]){
+                int hold = A[a-1];
+                A[a-1] = A[b];
+                A[b] = hold;
+            }
+            if(A[a-1]<A[a]) {
+                int hold = A[a];
+                A[a] = A[a - 1];
+                A[a - 1] = hold;
+            }
+            if(A[c-1]<A[c]) {
+                int hold = A[c];
+                A[c] = A[c - 1];
+                A[c - 1] = hold;
+            }
+            int first = A[a]*100 + A[b]*10 + A[c];
+            int second = A[a-1]*100 + A[b-1]*10 + A[c-1];
+
+            return first + second;
+        }*/
+
+
+
     }
+
+
+    public static int findPlace(int a, int[]B) {
+        if(a< B[3]){
+            if(a< B[2]){
+                if(a< B[1]){
+                    if(a< B[0]){
+                        B[0] = a;
+                        return 0;
+                    }
+                    B[1] = a;
+                    return 1;
+                }
+                B[2] = a;
+                return 2;
+            }
+            B[3] = a;
+            return 3;
+        }
+        return -1;
+    }
+
+
+
+
 }
+
+/*
+
+
+    int[] B = new int[4];
+        for(int p = 0; p < 4; p++){
+        B[p] = 10;
+        }
+
+        if(A.length < 5){
+        int sum = 0;
+        for(int k = 0; k<A.length; k++){
+        sum = A[k] +sum;
+        }
+        return sum;
+        }
+        for(int i = 0; i<A.length; i++){
+        int place = findPlace(A[i], B);
+
+        if(place != -1 && B[place] == 10){
+        B[place] = A[i];
+        }
+        if(place != -1 && B[place] !=10){
+        int holder = B[place];
+        B[place] = A[i];
+        findPlace(holder, B);
+        }
+
+        //makes it so the largest is in the back and the smallest in the front
+        if(A[i]< B[3]){
+        if(A[i]< B[2]){
+        if(A[i]< B[1]){
+        if(A[i]< B[0]){
+        B[0] = A[i];
+        continue;
+        }
+        B[1] = A[i];
+        continue;
+        }
+        B[2] = A[i];
+        continue;
+        }
+        B[3] = A[i];
+        continue;
+        }
+
+        }*/
